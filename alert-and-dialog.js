2b2ed53644a77d961b68562f3347f22f28f318e5
Mário de Lima <mariodelimadobrasil@gmail.com>
2022-09-09 16:04:40 -0300

    Create alert-and-dialog.js

---
+++ alert-and-dialog.js
@@ -0,0 +1,15 @@
+function menuItem1() {
+  ui.alert('⚠️ Title Alert 1!', 'You clicked the first menu item!', ui.ButtonSet.OK);
+}
+
+function menuItem2() {
+  ui.alert('⚠️ Title Alert 1!', 'You clicked the second menu item!', ui.ButtonSet.OK);
+}
+
+function showDialog() {
+  var html = HtmlService.createHtmlOutputFromFile('index')
+    .setWidth(400)
+    .setHeight(300);
+  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+    .showModalDialog(html, 'My custom dialog');
+}
