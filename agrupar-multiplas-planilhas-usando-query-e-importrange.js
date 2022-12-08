a04c778c3080efeb46a53a153393e20ddb17746e
Mário de Lima <mariodelimadobrasil@gmail.com>
2022-12-08 11:13:37 -0300

    Create agrupar-multiplas-planilhas-usando-query-e-importrange.js

---
+++ agrupar-multiplas-planilhas-usando-query-e-importrange.js
@@ -0,0 +1,34 @@
+=QUERY(
+{
+QUERY(IMPORTRANGE("11hVJcShvnuwCaPsP34MScJUftwLPy13fJoOngrocdIQ";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1twpF2iOREcXCIg5RNPFgluivNLbf8t-ECf0QBprC-UI";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1a550S3xQty6aHUYE8ayytDxlaeaHwFHVUTHwC9gHO70";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1t23ToP87xDj0Tzu3Iey1VDk6cwGIazKtPxDRsK5YXXM";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1wOsSC2Xz6OLeA8483wlfz2HotH3W7ytjwSOFB90HqA8";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1XQh5SbhxDShrmi6k0IexKoeYPCM21BY0OGdq6v-peeA";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1LqTJ3BdUUh-BV-DG5QejW4tMPIpevdYs8KXE2w_zqNY";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1yXJKJG5oCWjrVKiACs3ns_LQdmTd8KLIgV7AXpgKKJc";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1a6pREXelKwh8E1o4Iw_BUq69dUcDKNI_ms-hpQ1xNXE";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("156-dN6amvH9ZF3tF8q9Aj7NVKMJukIv21uWsEHqVoss";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1XygrT_SnttQlBWVjz6mf3u1RnwnhQ4o5vHnQEb8dWqo";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1kjK0DDvd3WPtQJvYS6vt3Xw2Pm52UZyuwaOeDvi68B0";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1J20ne9dykIsQQE0EPTNDpEJchYF7FbV3viPhYjMGAJw";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1Ovyk06LuqZXx2YRNtxoPwwo49zz8FnZilQre8QOvmN4";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1-VJYg2shfEOAsC4fqc49FY_FbNL8cZPBQw9cZI8Xhjk";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1LNDRpT_tV0Cl9Gfj7aARiKIwiTIHqbinR4rTloIysu0";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("16SFds6qsWRN_XXiWDI_w1cKGIt4X-Epldd0Lh3qN_HE";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1UkBIBB8p8VZTnsmq8q_62jZl1yWn1jEynHJEP7eoJbk";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1TcsBjjD1hjRqO2uWwAG_Q0-bWG2GLnwQsOFiwgM6Flc";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1hHBpZaYvwaAuVJ7Lq2rH6vgbtvp8O4C-VpPlvjYBheQ";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1srO6a7Xuzvgwu9Mnw03VXqWVeXBJqr4ybMp6OOqn8gw";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1yWEVQeL5ZWTMKzjnBLPLiZDTNDzFpgxMwCJmeAd6pSk";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1gsS6Lly2L7LpOt4decQaEf9JwfR4HHFY3eQb1FIYq3w";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1woXK5JTdDhniVp-S9JihX5DhtZOajxJIDaxlVjw06Gw";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1XYC17E1MBcUFZkxXMt1RbGerXzAo6qA_9jeroN0qnXI";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1lsVdgFwAMdZST9CHPqljaziNFByglLMcYzimeFEjgpc";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1PiC9qEzMVyMMSjPzBWljniV4grhe5ExRbvnpSItW9ws";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1_07B4douZC_ADEX-AXFubwQSN7iraeSWokbZn3Ee19k";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1pHKssTNJ-WSyxmikZnThNt_uGhJI4CHlkM7E7t13XU4";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30");
+QUERY(IMPORTRANGE("1PeE47XrPFsP5EoZM08lncMvaX-RoHeNG80nniuzfYto";"CONTROLE!A2:AD");"select Col1, Col2, Col4, Col8, Col9, Col13, Col12, Col27, Col28, Col29, Col30")
+};
+"SELECT * WHERE Col1 IS NOT NULL and Col2!='' ";0)
